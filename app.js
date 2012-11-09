f58d76c50d2643a53f7a7d47c75404b96642286a
Rafael Bandeira <rafaelbandeira3@gmail.com>
2012-11-09 20:42:22 -0300

    Usando connect para composição do servidor--middlewares

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var http = require('http');
 var connect = require('connect');
 var parseUrl  = require('url').parse;
 
-var nodeServer = function (req, res) {
-  var url = parseUrl(req.url);
+connect()
+  .use(function (req, res) {
+    var url = parseUrl(req.url);
 
-  res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-  res.end('Opa! Esse é o conteúdo da <strong>' + url.pathname + '</strong>');
-}
+    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    res.end('Opa! Esse é o conteúdo da <strong>' + url.pathname + '</strong>');
+  })
+  .listen(3000, '127.0.0.1');
 
-http.createServer(nodeServer).listen(3000, '127.0.0.1');
 console.log('Rodando o servidor em http://localhost:3000/');
