80a538ee1e7c35c63750f4e8fbca424882812217
Rafael Bandeira <rafaelbandeira3@gmail.com>
2012-11-09 20:29:33 -0300

    Agora com HTML

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var parseUrl  = require('url').parse;
 var nodeServer = function (req, res) {
   var url = parseUrl(req.url);
 
-  res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
-  res.end('Opa! Esse é o conteúdo da ' + url.pathname);
+  res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+  res.end('Opa! Esse é o conteúdo da <strong>' + url.pathname + '</strong>');
 }
 
 http.createServer(nodeServer).listen(3000, '127.0.0.1');
